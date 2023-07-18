30f923ef2c3bdcb1aaaaefb86cfd9140b7eda35a
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-18 09:58:11 +0100

    removed the qr code i

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -74,6 +74,74 @@ function Account() {
     }
   };
 
+  const handleMathsAttendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'maths');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'maths'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+  const handleEnglishAttendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'english');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'english'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
   if (user) {
     return (
       <div className='max-w-[1140px] mx-auto h-screen'>
@@ -90,17 +158,30 @@ function Account() {
             </button>
           </div>
         </div>
+
         <div className='flex flex-col md:flex-row pt-10 justify-center items-center'>
           <div className='w-10/12 sm:w-8/12 md:w-5/12 lg:w-6/12 flex flex-col justify-center items-center px-4'>
             <p className='text-4xl lg:text-5xl font-bold pt-10 md:pt-0'>Scan Your Presence</p>
-            <p className='lg:text-lg 2xl:text-xl py-6 text-center'>Click the button to be recognized as present</p>
-            <img src={QrImg} alt='' className='rounded-sm w-6/12 sm:w-4/12 md:w-5/12 lg:w-6/12' />
+           <p className='lg:text-lg 2xl:text-xl py-6 text-center'>Click the button to be recognized as present</p>
 
-            <Link className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2' onClick={handleAttendance}>
-              Click Here
-            </Link>
+           <div className='flex items-center justify-between mb-8'>
+<div className='text-left'>Mathematics</div>
+<div className='text-right'>
+  <Link className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2' onClick={handleMathsAttendance}>Click Here</Link>
+</div>
+</div>
+
+<div className='flex items-center justify-between mb-8'>
+<div className='text-left'>English</div>  
+<div className='text-right'>
+<Link className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2' onClick={handleEnglishAttendance}>Click Here</Link>
+</div>
+</div>
+
+
           </div>
         </div>
+
       </div>
     );
   }
