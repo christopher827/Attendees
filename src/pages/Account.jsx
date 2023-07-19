40f2758ec911b8f3df52efc6b8be2700ba0b61e4
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-19 02:39:15 +0100

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -74,7 +74,7 @@ function Account() {
     }
   };
 
-  const handleMathsAttendance = async()=>{
+  const handleAna302Attendance = async()=>{
     if (attendanceTaken) {
       alert('Your attendance for today has been marked, till tomorrow');
       return;
@@ -86,12 +86,12 @@ function Account() {
       });
       const { latitude, longitude } = position.coords;
 
-      const attendanceRef = collection(db, 'maths');
+      const attendanceRef = collection(db, 'ana302');
       const q = query(attendanceRef, where('email', '==', email), limit(1));
       const querySnapshot = await getDocs(q);
 
       if (querySnapshot.empty) {
-        await addDoc(collection(db, 'maths'), {
+        await addDoc(collection(db, 'ana302'), {
           email,
           latitude,
           longitude,
@@ -108,7 +108,7 @@ function Account() {
 
   }
 
-  const handleEnglishAttendance = async()=>{
+  const handleEnt302Attendance = async()=>{
     if (attendanceTaken) {
       alert('Your attendance for today has been marked, till tomorrow');
       return;
@@ -120,12 +120,12 @@ function Account() {
       });
       const { latitude, longitude } = position.coords;
 
-      const attendanceRef = collection(db, 'english');
+      const attendanceRef = collection(db, 'ent302');
       const q = query(attendanceRef, where('email', '==', email), limit(1));
       const querySnapshot = await getDocs(q);
 
       if (querySnapshot.empty) {
-        await addDoc(collection(db, 'english'), {
+        await addDoc(collection(db, 'ent302'), {
           email,
           latitude,
           longitude,
@@ -141,6 +141,213 @@ function Account() {
     }
 
   }
+
+
+  const handleGst207Attendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'Gst207');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'Gst207'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+  const handlePol201Attendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'Pol201');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'Pol210'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+  const handlePhs234Attendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'Phs234');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'Phs234'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+  const handleBch202Attendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'Bch202');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'Bch202'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+  const handlePha302Attendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'Pha302');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'Pha302'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+  const handlePth304Attendance = async()=>{
+    if (attendanceTaken) {
+      alert('Your attendance for today has been marked, till tomorrow');
+      return;
+    }
+    try {
+      const { email } = user;
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+
+      const attendanceRef = collection(db, 'Pth304');
+      const q = query(attendanceRef, where('email', '==', email), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        await addDoc(collection(db, 'Pth304'), {
+          email,
+          latitude,
+          longitude,
+        });
+
+        alert('Your attendance has been taken.');
+        setAttendanceTaken(true);
+      } else {
+        alert('Your attendance for today has already been marked.');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+
+
 
   if (user) {
     return (
@@ -164,21 +371,70 @@ function Account() {
             <p className='text-4xl lg:text-5xl font-bold pt-10 md:pt-0'>Scan Your Presence</p>
            <p className='lg:text-lg 2xl:text-xl py-6 text-center'>Click the button to be recognized as present</p>
 
-           <div className='flex items-center justify-between mb-8'>
-<div className='text-left'>Mathematics</div>
-<div className='text-right'>
-  <Link className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2' onClick={handleMathsAttendance}>Click Here</Link>
-</div>
-</div>
+           <div class="w-full text-center">
+  <div class="flex justify-between border-b">
+    <div class="text-left">Courses</div>
+    <div>Attendance</div>
+  </div>
 
-<div className='flex items-center justify-between mb-8'>
-<div className='text-left'>English</div>  
-<div className='text-right'>
-<Link className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2' onClick={handleEnglishAttendance}>Click Here</Link>
-</div>
-</div>
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">Ana 302</div>
+    <div>
+      <Link className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handleAna302Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">Ent 302</div>
+    <div>
+      <Link  className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handleEnt302Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">Gst 207</div>
+    <div>
+      <Link  className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handleGst207Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">Pol 201</div>
+    <div>
+      <Link  className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handlePol201Attendance}>Click Here</Link>
+    </div>
+  </div>
 
 
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">PHS 234</div>
+    <div>
+      <Link className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handlePhs234Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">BCH 202</div>
+    <div>
+  <Link className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handleBch202Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">PHA 302</div>
+    <div>
+  <Link  className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handlePha302Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+  <div className="flex items-center justify-between border-b p-3">
+    <div className="text-left">PTH 304</div>
+    <div>
+      <Link className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handlePth304Attendance}>Click Here</Link>
+    </div>
+  </div>
+
+</div>
           </div>
         </div>
 
