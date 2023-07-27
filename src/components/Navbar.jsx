5d5f1bce3c62a3352d5dbecc64cb9885cfdcd8d1
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-28 00:37:31 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ return (
 <Link to="/account" className='p-4'>Account</Link>
 <button onClick={handleSignOut}>Sign Out</button>
 </div>
-  ):(
+):(
   <div className='hidden md:block'>
   <Link to='/lecturersSignIn' className='p-4 hover:text-accent'>Lecturer</Link>
   <Link to='/signup' className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl '>Students</Link>
