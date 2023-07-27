26359c69abd3b502d225ff062b66dc27a7ca3667
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-28 00:38:35 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ return (
 ):(
 <div className='hidden md:block'>
 <Link to='/lecturersSignIn' className='p-4 hover:text-accent'>Lecturer</Link>
-  <Link to='/signup' className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl '>Students</Link>
+<Link to='/signup' className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl '>Students</Link>
 </div>  
 )}
 
