122b3f71c704b17adc125b7af3764e258149a79d
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-29 22:39:02 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ export const firebaseConfig = {
 apiKey: "AIzaSyDkNsAdr7gMs8LfKAQDgsmBiOTmbFpVjNg",
   authDomain: "attendies-2efdd.firebaseapp.com",
   projectId: "attendies-2efdd",
-  storageBucket: "attendies-2efdd.appspot.com",
+storageBucket: "attendies-2efdd.appspot.com",
 messagingSenderId: "443030887286",
 appId: "1:443030887286:web:8da78da8c6c09751927857",
 measurementId: "G-WECXEHSGFD"
