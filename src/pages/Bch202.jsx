4dd5082e4ef7604b58b25a3324dddbd36f8f1ba0
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-31 23:58:25 +0100

    Update Bch202.jsx

--- src/pages/Bch202.jsx
+++ src/pages/Bch202.jsx
@@ -3,7 +3,7 @@ import { collection, query, onSnapshot ,doc,updateDoc} from 'firebase/firestore'
 import { db } from '../firebase';
 
 function Bch202() {
-  const [students, setStudents] = useState([]);
+const [students, setStudents] = useState([]);
   const [lecturerLatitude, setLecturerLatitude] = useState(null);
   const [lecturerLongitude, setLecturerLongitude] = useState(null);
 
