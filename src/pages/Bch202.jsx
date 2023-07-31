e1e80a2e8ff8a3cc96282e1e6eb99c4a5c28b396
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-31 21:46:00 +0100

    Update Bch202.jsx

--- src/pages/Bch202.jsx
+++ src/pages/Bch202.jsx
@@ -133,7 +133,7 @@ const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       <button  className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl ' onClick={handleDeletion}>Delete Attendance</button>
 </div>
 
-    </div>
+</div>
 );
 }
 export default Bch202;
