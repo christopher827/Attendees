ed8ec864bfb85a764f5b1c0c84afbf6109563d92
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-07-31 21:45:04 +0100

    Update Bch202.jsx

--- src/pages/Bch202.jsx
+++ src/pages/Bch202.jsx
@@ -34,7 +34,7 @@ console.error('Error getting lecturer location:', error);
           Math.cos(toRadians(lat2)) *
           Math.sin(dLon / 2) *
           Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       const distance = R * c;
       return distance;
     } else {
