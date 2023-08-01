7a4b69a545dcba3d7260fd59a2f3bf1fefbfd2f1
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-01 21:50:16 +0100

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -417,7 +417,7 @@ const handlePth304Attendance = async()=>{
     <div>
   <Link className="bg-blue-500 text-white px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl mt-2" onClick={handleBch202Attendance}>Click Here</Link>
     </div>
-  </div>
+</div>
 
   <div className="flex items-center justify-between border-b p-3">
     <div className="text-left">PHA 302</div>
