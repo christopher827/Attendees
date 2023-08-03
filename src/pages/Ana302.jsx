72f893507cff4c1fe61c4107bded8148969f0e20
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-03 19:20:54 +0100

    Update Ana302.jsx

--- src/pages/Ana302.jsx
+++ src/pages/Ana302.jsx
@@ -29,7 +29,7 @@ const R = 6371; // Radius of the Earth in kilometers
 const dLat = toRadians(lat2 - lat1);
 const dLon = toRadians(lon2 - lon1);
 const a=Math.sin(dLat / 2) * Math.sin(dLat / 2)+Math.cos(toRadians(lat1))*Math.cos(toRadians(lat2))*Math.sin(dLon / 2)*Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       const distance = R * c;
       return distance;
     } else {
