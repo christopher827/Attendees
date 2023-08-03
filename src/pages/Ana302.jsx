0edeeb840bcc1d33335ef9bf845c8cfc0fa4b1d9
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-03 19:26:51 +0100

    Update Ana302.jsx

--- src/pages/Ana302.jsx
+++ src/pages/Ana302.jsx
@@ -46,7 +46,7 @@ const fetchStudents = () => {
 const studentsRef = collection(db, 'ana302');
 const q = query(studentsRef);
 return onSnapshot(q, (snapshot) => {
-      const studentData = snapshot.docs.map((doc) => ({
+const studentData = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
