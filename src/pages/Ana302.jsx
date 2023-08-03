f9794b95a20220dbb621ebbd1b82c3b661286188
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-03 02:48:35 +0100

    Update Ana302.jsx

--- src/pages/Ana302.jsx
+++ src/pages/Ana302.jsx
@@ -17,7 +17,7 @@ setLecturerLongitude(position.coords.longitude);
 (error) => {
 console.error('Error getting lecturer location:', error);
 }
-      );
+);
     } else {
       console.error('Geolocation is not supported by this browser.');
     }
