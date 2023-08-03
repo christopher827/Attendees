35b0c6cb23a35ced35b65d7f23d9e8cd892df90b
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-03 02:52:00 +0100

    Update Ana302.jsx

--- src/pages/Ana302.jsx
+++ src/pages/Ana302.jsx
@@ -28,7 +28,7 @@ if (lat1 && lon1 && lat2 && lon2) {
 const R = 6371; // Radius of the Earth in kilometers
 const dLat = toRadians(lat2 - lat1);
 const dLon = toRadians(lon2 - lon1);
-const a =    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+const a =   Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(toRadians(lat1)) *
           Math.cos(toRadians(lat2)) *
           Math.sin(dLon / 2) *
