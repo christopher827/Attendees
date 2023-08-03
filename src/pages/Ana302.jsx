475bd7f9c78ff133130ea262704441b6577e27fd
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-03 19:21:31 +0100

    Update Ana302.jsx

--- src/pages/Ana302.jsx
+++ src/pages/Ana302.jsx
@@ -31,7 +31,7 @@ const dLon = toRadians(lon2 - lon1);
 const a=Math.sin(dLat / 2) * Math.sin(dLat / 2)+Math.cos(toRadians(lat1))*Math.cos(toRadians(lat2))*Math.sin(dLon / 2)*Math.sin(dLon / 2);
 const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 const distance = R * c;
-      return distance;
+return distance;
     } else {
       return null;
     }
