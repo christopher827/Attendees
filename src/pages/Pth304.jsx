d3811ca0cc427f40f45aa613564c4ca15f53a61d
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-24 00:04:42 +0100

    Update Pth304.jsx

--- src/pages/Pth304.jsx
+++ src/pages/Pth304.jsx
@@ -32,7 +32,7 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
         Math.cos(toRadians(lat1)) *
           Math.cos(toRadians(lat2)) *
           Math.sin(dLon / 2) *
-          Math.sin(dLon / 2);
+        Math.sin(dLon / 2);
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       const distance = R * c;
       return distance;
