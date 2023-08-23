28cb3c1376468fd98a512efb60c38f9490fa19e3
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-08-23 23:58:38 +0100

    Update Pth304.jsx

--- src/pages/Pth304.jsx
+++ src/pages/Pth304.jsx
@@ -5,7 +5,7 @@ import { db } from '../firebase';
 function Pth304() {
 const [students, setStudents] = useState([]);
 const [lecturerLatitude, setLecturerLatitude] = useState(null);
-  const [lecturerLongitude, setLecturerLongitude] = useState(null);
+const [lecturerLongitude, setLecturerLongitude] = useState(null);
 
   const getLecturerLocation = () => {
     if (navigator.geolocation) {
