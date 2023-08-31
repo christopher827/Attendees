ff7278f950354608f1c4ea2415be2b9f2976444b
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-09-01 00:00:56 +0100

    Update Pol201.jsx

--- src/pages/Pol201.jsx
+++ src/pages/Pol201.jsx
@@ -29,7 +29,7 @@ function Pol201() {
   const dLat = toRadians(lat2 - lat1);
   const dLon = toRadians(lon2 - lon1);
   const a =    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRadians(lat1)) *
+      Math.cos(toRadians(lat1)) *
           Math.cos(toRadians(lat2)) *
           Math.sin(dLon / 2) *
           Math.sin(dLon / 2);
