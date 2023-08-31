7320e0760e0d01fbd859212942c72035285eb949
Christopher Oche <104537376+christopher827@users.noreply.github.com>
2023-09-01 00:12:51 +0100

    Update Pol201.jsx

--- src/pages/Pol201.jsx
+++ src/pages/Pol201.jsx
@@ -28,7 +28,7 @@ function Pol201() {
   const R = 6371; // Radius of the Earth in kilometers
   const dLat = toRadians(lat2 - lat1);
   const dLon = toRadians(lon2 - lon1);
-  const a=Math.sin(dLat / 2)* Math.sin(dLat / 2)+ Math.cos(toRadians(lat1))*Math.cos(toRadians(lat2))*     Math.sin(dLon / 2) *
+  const a=Math.sin(dLat / 2)* Math.sin(dLat / 2)+ Math.cos(toRadians(lat1))*Math.cos(toRadians(lat2))*    Math.sin(dLon / 2) *
           Math.sin(dLon / 2);
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       const distance = R * c;
